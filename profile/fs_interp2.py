7f6cdfa2f19d297c631f35ce6b35f3822c29ca48
Eric B <eric.bezzam@epfl.ch>
2021-04-16 17:29:20 +0200

    Profile interp against naive approach.

--- profile/fs_interp2.py
+++ profile/fs_interp2.py
@@ -12,6 +12,40 @@ from pyffs.interp import fs_interpn
 from pyffs.backend import AVAILABLE_MOD, get_module_name
 
 
+def naive_interp2d(diric_FS, T, a, b, M):
+
+    # create sample points
+    D = len(T)
+    sample_points = []
+    for d in range(D):
+        sh = [1] * D
+        sh[d] = M[d]
+        sample_points.append(
+            np.linspace(start=a[d], stop=b[d], num=M[d], endpoint=False).reshape(sh)
+        )
+
+    # initialize output
+    x_vals = np.linspace(start=a[0], stop=b[0], num=M[0], endpoint=False)
+    y_vals = np.linspace(start=a[1], stop=b[1], num=M[1], endpoint=False)
+    output_shape = (len(x_vals), len(y_vals))
+    vals = np.zeros(output_shape, dtype=complex)
+
+    # loop to avoid creating potentially large matrices
+    N_FSx, N_FSy = diric_FS.shape
+    Kx = N_FSx // 2
+    Ky = N_FSy // 2
+    fsx_idx = np.arange(-Kx, Kx + 1)[:, np.newaxis]
+    fsy_idx = np.arange(-Ky, Ky + 1)[np.newaxis, :]
+    for i, _x_val in enumerate(x_vals):
+        for j, _y_val in enumerate(y_vals):
+            vals[i, j] = np.sum(
+                diric_FS
+                * np.exp(1j * 2 * np.pi * fsx_idx / T[0] * _x_val)
+                * np.exp(1j * 2 * np.pi * fsy_idx / T[1] * _y_val)
+            )
+    return vals
+
+
 @click.command()
 @click.option("--n_trials", type=int, default=10)
 def profile_fs_interp2(n_trials):
@@ -32,6 +66,18 @@ def profile_fs_interp2(n_trials):
         print("\nNumber of FS coefficients : {}".format(N_FS))
         proc_time[N_FS] = dict()
         proc_time_std[N_FS] = dict()
+
+        # naive approach
+        diric_FS = np.outer(dirichlet_fs(N_FS, T, T_c, mod=np), dirichlet_fs(N_FS, T, T_c, mod=np))
+        _key = "naive"
+        timings = []
+        for _ in range(n_trials):
+            start_time = time.time()
+            naive_interp2d(diric_FS, [T, T], [a, a], [b, b], [M, M])
+            timings.append(time.time() - start_time)
+        proc_time[N_FS][_key] = np.mean(timings)
+        proc_time_std[N_FS][_key] = np.std(timings)
+        print("-- {} : {} seconds".format(_key, proc_time[N_FS][_key]))
 
         # Loop through modules
         for mod in AVAILABLE_MOD:
