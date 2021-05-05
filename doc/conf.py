9e627697460e47f01592d551047ceb4bf93f0f33
Eric B <eric.bezzam@epfl.ch>
2021-05-05 15:05:54 +0200

    Add pyffs to autodoc_moc_imports.

--- doc/conf.py
+++ doc/conf.py
@@ -11,7 +11,7 @@ import pathlib
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join("..", "pyffs")))
-autodoc_mock_imports = ["numpy", "scipy"]
+autodoc_mock_imports = ["numpy", "scipy", "pyffs"]
 
 
 def setup_config() -> configparser.ConfigParser:
