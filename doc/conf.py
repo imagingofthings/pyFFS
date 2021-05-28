014d7fa0fe7881f69f90ac4cd35e949222547092
Eric B <eric.bezzam@epfl.ch>
2021-05-28 17:43:38 +0200

    Try without relative import of pyFFS for doc.

--- doc/conf.py
+++ doc/conf.py
@@ -11,8 +11,8 @@ import pathlib
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(".."))
-sys.path.insert(0, os.path.abspath(os.path.join("..", "pyffs")))
+# sys.path.insert(0, os.path.abspath(".."))
+# sys.path.insert(0, os.path.abspath(os.path.join("..", "pyffs")))
 autodoc_mock_imports = ["numpy", "scipy"]
 
 
